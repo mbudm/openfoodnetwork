b2a55ea7b08c7caa98d418088f1ed8651b8c162d
Luis Ramos <luisramos0@gmail.com>
2020-06-10 12:59:27 +0100

    Make rswag spec work

--- spec/requests/api/orders_spec.rb
+++ spec/requests/api/orders_spec.rb
@@ -1,24 +1,29 @@
+# frozen_string_literal: true
+
 require 'swagger_helper'
 
-RSpec.describe 'api/orders', type: :request do
+describe 'api/orders', type: :request do
   path '/api/orders' do
-
     get('list orders') do
       tags 'Order Tests'
-      security [{ api_key: [] }]
+      parameter name: 'X-Spree-Token', in: :header, type: :string
 
       response(200, 'successful') do
-        let(:api_key) { 'some key' }
-        
-          before do |example|
-            binding.pry
-            submit_request(example.metadata)
-          end
-          
-          it do
-            binding.pry
-            expect(response).to have_http_status(200)
-          end
+        let(:order) { create(:order_with_distributor) }
+        let(:user) { order.distributor.owner }
+        let(:'X-Spree-Token') do
+          user.generate_spree_api_key!
+          user.spree_api_key
+        end
+
+        run_test! do |response|
+          expect(response).to have_http_status(200)
+
+          data = JSON.parse(response.body)
+          orders = data["orders"]
+          expect(orders.size).to eq 1
+          expect(orders.first["id"]).to eq order.id
+        end
       end
     end
   end
