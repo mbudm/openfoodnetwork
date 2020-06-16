a00459f99e945076dd689d670536e67210b0c9fb
Steve Roberts <steve@commoncode.io>
2020-06-16 15:04:28 +1000

    valid swagger 3 spec and working test WIP

--- spec/requests/api/orders_spec.rb
+++ spec/requests/api/orders_spec.rb
@@ -5,16 +5,34 @@ require 'swagger_helper'
 describe 'api/orders', type: :request do
   path '/api/orders' do
     get('list orders') do
-      tags 'Order Tests'
-      parameter name: 'X-Spree-Token', in: :header, type: :string
-
-      response(200, 'successful') do
-        let(:order) { create(:order_with_distributor) }
+      tags 'Orders'
+      parameter name: 'X-Spree-Token', in: :header, schema: {type: :string}
+      parameter name: 'q[distributor_id_eq]', in: :query, schema: {type: :string}, style: :deepObject, description: "Query orders for a specific distributor id."
+      parameter name: 'q[completed_at_gt]', in: :query, schema: {type: :string}, style: :deepObject, description: "Query orders completed after a date."
+      parameter name: 'q[completed_at_lt]', in: :query, schema: {type: :string}, style: :deepObject, description: "Query orders completed before a date."
+      parameter name: 'q[state_eq]', in: :query, schema: {type: :string}, style: :deepObject, description: "Query orders by order state, eg 'cart', 'complete'."
+      parameter name: 'q[payment_state_eq]', in: :query, schema: {type: :string}, style: :deepObject, description: "Query orders by order payment_state, eg 'balance_due', 'paid', 'failed'."
+      parameter name: 'q[email_cont]', in: :query, schema: {type: :string}, style: :deepObject, description: "Query orders where the order email contains a string."
+      parameter name: 'q[order_cycle_id_eq]', in: :query, schema: {type: :string}, style: :deepObject, description: "Query orders for a specific order_cycle id."
+      
+      response(200, 'get orders') do
+        # Adds model metadata for Swagger UI. Ideally we'd be able to just add:
+        # schema '$ref' => '#/components/schemas/Order_Concise'
+        # Which would also validate the response in the test, however this is an open 
+        # issue with rswag: https://github.com/rswag/rswag/issues/268
+        metadata[:response][:content] = { "application/json": {
+            schema: {'$ref' => '#/components/schemas/Order_Concise'}
+          }
+        }
+        
+        let(:order_1) { create(:order_with_distributor) }
+        let(:order_2) { create(:order_with_distributor) }
         let(:user) { order.distributor.owner }
         let(:'X-Spree-Token') do
           user.generate_spree_api_key!
           user.spree_api_key
         end
+        let(:'q[distributor_id_eq]') { order_1.distributor.id }
 
         run_test! do |response|
           expect(response).to have_http_status(200)
--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -8,27 +8,62 @@ RSpec.configure do |config|
     'v1/swagger.yaml' => {
       openapi: '3.0.1',
       info: {
-        title: 'API V1',
-        version: 'v1'
+        title: 'The Open Food Network',
+        description: 'Some endpoints are public and require no authorization; others require authorization. Talk to us to get your credentials set up. Check out our repo! https://github.com/openfoodfoundation/openfoodnetwork',
+        version: '0.1',
       },
       components: {
         securitySchemes: {
           api_key: {
               type: :apiKey,
-              name: 'api_key',
-              in: :query
+              name: 'X-Spree-Token',
+              in: :header
+          }
+        },
+        schemas: {
+          Order_Concise: {
+            type: 'object',
+            properties: {
+              id: { type: 'integer' },
+              number: { type: 'string' },
+              full_name: { type: 'string' },
+              email: { type: 'string' },
+              phone: { type: 'string' },
+              completed_at: { type: 'string' },
+              display_total: { type: 'string' },
+              show_path: { type: 'string' },
+              edit_path: { type: 'string' },
+              state: { type: 'string' },
+              payment_state: { type: 'string' },
+              shipment_state: { type: 'string' },
+              payments_path: { type: 'string' },
+              shipments_path: { type: 'string' },
+              ship_path: { type: 'string' },
+              ready_to_ship: { type: 'string' },
+              created_at: { type: 'string' },
+              distributor_name: { type: 'string' },
+              special_instructions: { type: 'string' },
+              payment_capture_path: { type: 'string' },
+              distributor: {
+                type: 'object',
+                properties: {
+                  id: { type: 'integer' }
+                }
+              },
+              order_cycle: {
+                type: 'object',
+                properties: {
+                  id: { type: 'integer' }
+                }
+              }
+            }
           }
         }
       },
       paths: {},
       servers: [
         {
-          url: 'https://{defaultHost}',
-          variables: {
-            defaultHost: {
-              default: '0.0.0.0:3000/'
-            }
-          }
+          url: 'https://staging.katuma.org/api'
         }
       ]
     }
